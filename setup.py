9690a7fd6980a491bd4c6757e44335b9ef7ea9e2
jarbasai <jarbasai@mailfence.com>
2022-03-02 02:28:32 +0000

    feat/workflows

--- setup.py
+++ setup.py
@@ -1,8 +1,49 @@
 #!/usr/bin/env python3
 from setuptools import setup
+import os
+
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
 
 # skill_id=package_name:SkillClass
-PLUGIN_ENTRY_POINT = 'mycroft-volume.mycroftai=ovos_skill_volume:VolumeSkill'
+PLUGIN_ENTRY_POINT = 'ovos-skill-volume.OpenVoiceOS=ovos_skill_volume:VolumeSkill'
 # in this case the skill_id is defined to purposefully replace the mycroft version of the skill,
 # or rather to be replaced by it in case it is present. all skill directories take precedence over plugin skills
 
@@ -10,7 +51,7 @@ PLUGIN_ENTRY_POINT = 'mycroft-volume.mycroftai=ovos_skill_volume:VolumeSkill'
 setup(
     # this is the package name that goes on pip
     name='ovos-skill-volume',
-    version='0.0.1',
+    version=get_version(),
     description='OVOS volume skill plugin',
     url='https://github.com/OpenVoiceOS/skill-ovos-volume',
     author='JarbasAi',
@@ -20,7 +61,7 @@ setup(
     package_data={'ovos_skill_volume': ["locale/*"]},
     packages=['ovos_skill_volume'],
     include_package_data=True,
-    install_requires=["pyalsaaudio==0.8.2"],
+    install_requires=required("requirements.txt"),
     keywords='ovos skill plugin',
     entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
 )
